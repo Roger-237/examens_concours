59cda31184d9780b085befa424aebe4475fc0ccb
Roger-237 <pendingpending373.com>
2026-08-06 22:14:07 +0100

    Fix: suppression du mode WAL SQLite pour PythonAnywhere

--- config/settings.py
+++ config/settings.py
@@ -73,7 +73,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME'  : BASE_DIR / 'db.sqlite3',
         'OPTIONS': {
-            'init_command': 'PRAGMA journal_mode=WAL;',
             'timeout': 20,
         },
     }
